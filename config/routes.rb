285d75ae974484b3b3375cdf3d97ce39d3fef7e8
Jimmy Li <li17.jimmy@gmail.com>
2015-07-28 19:43:07 +0000

    fix_index_layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
+  # CREATE
   get '/movies/new' => 'application#new'
   get '/movies/create' => 'application#create'
+  
+  # READ
   get '/movies/:id' => 'application#show'
-  get '/movies/:id/edit' => 'application#edit'
-  get '/update_movies/:id' => 'application#update'
   get '/movies' => 'application#index'
+  
+  # UPDATE
+  get '/update_movies/:id' => 'application#update'
+  get '/movies/:id/edit' => 'application#edit'
+  
+  # DESTROY
   get '/movies/:id/delete' => 'application#delete'
 end
